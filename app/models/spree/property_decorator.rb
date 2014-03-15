56217e35b04cf2ec22ee8833e301572f56e25f72
sstarke <sirsupertramp@gmail.com>
2014-03-15 22:54:42 +0100

    Updated to new globalize version.

--- app/models/spree/property_decorator.rb
+++ app/models/spree/property_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   Property.class_eval do
-    translates :name, :presentation, :fallbacks_for_empty_translations => true
+    translates :name, :presentation
     include SpreeI18n::Translatable
   end
 end
